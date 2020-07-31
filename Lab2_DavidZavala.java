a29ec41179aa6d3d09f42cd88aaabd88c70cf763
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-07-31 14:10:40 -0600

    Log In y sus validaciones

--- Lab2_DavidZavala.java
+++ Lab2_DavidZavala.java
@@ -5,17 +5,88 @@
  */
 package lab2_davidzavala;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 50431
  */
 public class Lab2_DavidZavala {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner read = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        char sn = 's';
+        boolean tf = false;
+        String usuario = "leobanegas", contrasena = "99";
+        while (sn == 's') {
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            System.out.println("4");
+            System.out.println("5");
+            System.out.println("6");
+            System.out.println("7");
+            System.out.println("0. Salir");
+            int op = read.nextInt();
+            switch (op) {
+                case 1: {
+                    if (tf == true) {
+
+                    }
+                    break;
+                }
+                case 2: {
+                    if (tf == true) {
+
+                    }
+                    break;
+                }
+                case 3: {
+                    while (tf) {
+                        System.out.println("Usuario");
+                        String temp1 = read.next();
+                        System.out.println("Contraseña");
+                        String temp2 = read.next();
+                        if (temp1.equals(usuario) && temp2.equalsIgnoreCase(temp2)) {
+                            tf = !tf;
+                        } else {
+                            System.out.println("Credenciales incorrectas");
+                        }
+
+                    }
+                    break;
+                }
+                case 4: {
+                    if (tf == true) {
+
+                    }
+                    break;
+                }
+                case 5: {
+                    if (tf == true) {
+
+                    }
+                    break;
+                }
+                case 6: {
+                    if (tf == true) {
+
+                    }
+                    break;
+                }
+                case 7: {
+                    if (tf == true) {
+
+                    }
+                    break;
+                }
+                case 0: {
+                    sn = 'n';
+                    break;
+                }
+                default:
+            }
+        }
     }
-    
 }
