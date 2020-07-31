1725ea90700665e2fb958dbe5b82a8e20275841f
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-07-31 16:27:34 -0600

    add delete y listar listo

--- Lab2_DavidZavala.java
+++ Lab2_DavidZavala.java
@@ -5,6 +5,7 @@
  */
 package lab2_davidzavala;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -14,25 +15,120 @@ import java.util.Scanner;
 public class Lab2_DavidZavala {
 
     static Scanner read = new Scanner(System.in);
+    static Empleados Emp = new Empleados();
+
+    static ArrayList All = new ArrayList();
+    static ArrayList Man = new ArrayList();
+    static ArrayList Ase = new ArrayList();
+    static ArrayList Caj = new ArrayList();
+    static ArrayList Seg = new ArrayList();
 
     public static void main(String[] args) {
         char sn = 's';
         boolean tf = false;
         String usuario = "leobanegas", contrasena = "99";
         while (sn == 's') {
-            System.out.println("1");
-            System.out.println("2");
-            System.out.println("3");
-            System.out.println("4");
-            System.out.println("5");
-            System.out.println("6");
-            System.out.println("7");
+            System.out.println("1. Registro de Emplados");
+            System.out.println("2. Despedir Empleados");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("5. ");
+            System.out.println("6. ");
+            System.out.println("7. ");
             System.out.println("0. Salir");
             int op = read.nextInt();
             switch (op) {
                 case 1: {
                     if (tf == true) {
+                        System.out.println("1. Nombre");
+                        String nombre = read.next();
+                        Emp.setNombre(nombre);
 
+                        System.out.println("2. Apellido ");
+                        String apellido = read.next();
+                        Emp.setApellido(apellido);
+
+                        System.out.println("3. Color Favorito ");
+                        String color = read.next();
+                        Emp.setColor(color);
+
+                        System.out.println("4. Edad");
+                        int edad = read.nextInt();
+                        Emp.setEdad(edad);
+
+                        System.out.println("5. Genero");
+                        System.out.println("1. Masculino ||| 2. Femenino");
+                        int gen = read.nextInt();
+                        boolean c = false;
+                        while (c) {
+                            switch (gen) {
+                                case 1:
+                                    Emp.setGenero("Masculino");
+                                    c = !c;
+                                    break;
+                                case 2:
+                                    Emp.setGenero("Femenino");
+                                    c = !c;
+                                    break;
+                                default:
+                                    System.out.println("Error intente de nuevo");
+                                    break;
+                            }
+                        }
+
+                        System.out.println("6. Altura");
+                        double altura = read.nextDouble();
+                        Emp.setAltura(altura);
+
+                        System.out.println("7. Peso");
+                        double peso = read.nextDouble();
+                        Emp.setPeso(peso);
+
+                        System.out.println("8. Titulo");
+                        System.out.println("1. Bachiller ||| 2. Licenciado ||| 3. Abogado ||| 4. Ingeniero ||| 5. Doctor");
+                        int tit = read.nextInt();
+                        boolean cc = false;
+                        while (cc) {
+                            switch (tit) {
+                                case 1:
+                                    Emp.setTitulo("Bachiller");
+                                    break;
+                                case 2:
+                                    Emp.setTitulo("Licenciado");
+                                    break;
+                                case 3:
+                                    Emp.setTitulo("Abogado");
+                                    break;
+                                case 4:
+                                    Emp.setTitulo("Ingeniero");
+                                    break;
+                                case 5:
+                                    Emp.setTitulo("Doctor");
+                                    break;
+                                default:
+                            }
+                        }
+                        System.out.println("9. Cargo");
+                        System.out.println("1. Gerente ||| 2. Aseador ||| 3. Cajero ||| 4. Seguridad");
+                        int car = read.nextInt();
+                        boolean ccc = false;
+                        while (ccc) {
+                            switch (car) {
+                                case 1:
+                                    Emp.setCargo("Gerente");
+                                    break;
+                                case 2:
+                                    Emp.setCargo("Aseador");
+                                    break;
+                                case 3:
+                                    Emp.setCargo("Cajero");
+                                    break;
+                                case 4:
+                                    Emp.setCargo("Seguridad");
+                                    break;
+                                default:
+                            }
+                        }
                     }
                     break;
                 }
@@ -65,7 +161,9 @@ public class Lab2_DavidZavala {
                 }
                 case 5: {
                     if (tf == true) {
-
+                        for (int i = 0; i < All.size(); i++) {
+                            System.out.println("[" + All.get(i) + "]");
+                        }
                     }
                     break;
                 }
